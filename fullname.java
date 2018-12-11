cdd62b332a6279a2223437f8ffde086e0cb4a2c9
juanlopezhernandez <lopezjuan0192@gmail.com>
2018-12-11 10:34:28 -0800

    first commit

---
+++ fullname.java
@@ -0,0 +1,14 @@
+
+public class fullname {
+   
+    public static void main(String args[]) {
+     String firstName;
+  firstName = "Juan";
+String lastName;
+  lastName = "Lopez Hernandez";
+String middleName;
+  middleName = "Martin";
+ String fullName = (firstName + " " + middleName + " " + lastName);
+  System.out.println(fullName);
+}      
+}
